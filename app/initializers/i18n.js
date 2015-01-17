a753c303b5150f68f397e39814e1213f9b4a3b4e
Kelly Selden <kellyselden@gmail.com>
2015-01-17 10:50:36 -0500

    set locale on start

--- app/initializers/i18n.js
+++ app/initializers/i18n.js
@@ -1,16 +1,22 @@
 import Ember from 'ember';
 
-export function initialize(container, application) {
-  var i18n = {};
+var i18n = {};
 
+function setLocale(locale) {
+  Ember.set(i18n, 'locale', locale);
+}
+
+export function initialize(container, application) {
   application.register('utils:i18n', i18n, { instantiate: false });
   application.inject('route', 'i18n', 'utils:i18n');
   application.inject('model', 'i18n', 'utils:i18n');
   application.inject('component', 'i18n', 'utils:i18n');
   application.inject('controller', 'i18n', 'utils:i18n');
 
+  setLocale(application.defaultLocale);
+
   application.localeStream.subscribe(function(stream) {
-    Ember.set(i18n, 'locale', stream.value());
+    setLocale(stream.value());
   });
 }
 
